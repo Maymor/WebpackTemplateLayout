0ed46226ea6aa8c3478d4016eed0a05bab8376c0
Gopher <djin85_85@mail.ru>
2018-10-15 21:19:16 +0300

    Day 2 clear!

---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+module.exports = () => {
+    const config = {
+      plugins: [
+        new ProgressBarPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new CompressionPlugin({
+          asset: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|css|html|svg)$/,
+          /* deleteOriginalAssets: true, */
+          threshold: 0,
+          minRatio: 0.8
+        })
+      ]
+    }
+    return merge(baseConfig, config)
+  }
